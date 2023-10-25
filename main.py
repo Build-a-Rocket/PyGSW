b0abc1d6a2a53d0d9fda74f87fd9922f674993ec
Brady Langdale <bradylangdale@gmail.com>
2023-10-25 17:42:53 -0500

    Add new serial_thread functionality

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import random
 
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 from PyQt6 import uic, QtCore
+from PyQt6.QtCore import QThread
+
+from serial import Serial, unicode
+from serial_thread import SerialThread
+
 
 
 class UI(QWidget):
@@ -17,8 +22,49 @@ class UI(QWidget):
 
         self.label = self.findChild(QLabel, 'label')
 
+        # Initiate serial port
+        #self.serial_port = Serial('COM18', 2000000, dsrdtr=True)
+        self.serial_port = '04-16-2023_16-25-07.csv'
+
+        # Initiate Serial Thread
+        self.serialThread = SerialThread(self.serial_port, fake=True)
+        self._thread = QThread()
+        self.serialThread.moveToThread(self._thread)
+
+        self.serialThread.connectionSuccess.connect(self.connection_success)
+        self.serialThread.connectionFailed.connect(self.connection_failed)
+        self.serialThread.readFailed.connect(self.error_on_read)
+
+        self._thread.started.connect(self.serialThread.run)
+
+        self.serialThread.dataReceived.connect(self.updateOutputBox)
+
+        self._thread.start()
+
     def buttonClick(self):
         self.label.setText(str(random.randint(0, 100)))
+
+    @QtCore.pyqtSlot()
+    def connection_success(self):
+        print('Connected!')
+
+    @QtCore.pyqtSlot(str)
+    def connection_failed(self, error):
+        print(error)
+
+    @QtCore.pyqtSlot(str)
+    def error_on_read(self, error):
+        print(error)
+
+    @QtCore.pyqtSlot(bytes)
+    def updateOutputBox(self, data):
+        print(unicode(data, errors='ignore'))
+
+    def closeEvent(self, event):
+        self.serialThread.stop()
+        self._thread.quit()
+        self._thread.wait()
+
 
 app = QApplication([])
 window = UI()
