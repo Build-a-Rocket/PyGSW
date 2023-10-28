cb6b6f5f22c087b9bd1998058ece115ced95b897
Shlok Bhakta <shlokbhakta1@gmail.com>
2023-10-27 20:06:14 -0500

    unicode

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 import random
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QTextEdit
 from PyQt6 import uic, QtCore
 from PyQt6.QtCore import QThread
 
 from serial_thread import SerialThread
+from serial import unicode
 
 class UI(QWidget):
     def __init__(self):
         super().__init__()
         # loading the ui file with uic module
-        uic.loadUi('tutorial.ui', self)
-        self.button = self.findChild(QPushButton, 'numberButton')
-        self.button.clicked.connect(self.buttonClick)
-        self.label = self.findChild(QLabel, 'label')
+        uic.loadUi('gsw.ui', self)
+        self.textBox = self.findChild(QTextEdit, 'outputBox')
 
         serial_port = "04-16-2023_16-25-07.csv"
         self.serialStream = SerialThread(serial_port, fake=True)
@@ -23,11 +22,16 @@ class UI(QWidget):
         self.myThread.started.connect(self.serialStream.run)
         self.serialStream.dataReceived.connect(self.printSerial)  
 
+        self.myThread.start()
+
 
     def buttonClick(self):
         self.label.setText(str(random.randint(0, 100)))
 
     def printSerial(self, data):
+        data = unicode(data, errors='ignore')
+
+        self.textBox.setText(data)
         print(data)
 
 app = QApplication([])
