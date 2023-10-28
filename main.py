14e534171c9fb7df6b605dd6dda4c26fb1e07083
Shlok Bhakta <shlokbhakta1@gmail.com>
2023-10-27 19:17:20 -0500

    added lastTimeCode

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+import random
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
 from PyQt6 import uic, QtCore
 
-
 class UI(QWidget):
-
     def __init__(self):
         super().__init__()
-
         # loading the ui file with uic module
-        uic.loadUi('UI File Path Here', self)
+        uic.loadUi('tutorial.ui', self)
+        self.button = self.findChild(QPushButton, 'numberButton')
+        self.button.clicked.connect(self.buttonClick)
+        self.label = self.findChild(QLabel, 'label')
+    def buttonClick(self):
+        self.label.setText(str(random.randint(0, 100)))
 
-#app run starts here
 app = QApplication([])
 window = UI()
 window.show()
